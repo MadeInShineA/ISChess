5b50fd37f827941a8d66f4fa9fa8bdf2a397be9c
MadeInShineA <olivier.amacker@netplus.ch>
2024-12-28 16:15:59 +0100

    Added arguments to AB_stats.py

--- AB_stats.py
+++ AB_stats.py
@@ -1,4 +1,4 @@
-
+import sys
 from multiprocessing.dummy import Pool as ThreadPool
 from PyQt6.QtWidgets import QApplication
 
@@ -9,9 +9,12 @@ def simulate_game(white_bot: str, black_bot: str, number_of_turns: int, time_per
     app.start(white_bot, black_bot, number_of_turns, time_per_turn)
 
 if __name__ == '__main__':
-    pool = ThreadPool(4)
+    args: list[str] = sys.argv
 
-    for i in range(5):
-        simulate_game("minmax_stats", "prunning_stats", 5, 2)
-
+    if len(args) == 1:
+        for i in range(5):
+            simulate_game("minmax_stats", "prunning_stats", 5, 2)
+    elif len(args) == 5:
+        white_bot, black_bot, number_of_turns, time_per_turn = args[1:]
+        simulate_game(white_bot, black_bot, int(number_of_turns), int(time_per_turn))
 
