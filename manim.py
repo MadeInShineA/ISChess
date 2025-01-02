514c1b80076a42cb9eff27a36ed642160599487b
MadeInShineA <olivier.amacker@netplus.ch>
2025-01-02 11:21:32 +0100

    Continued the MinMaxTree inside manim.py

--- manim.py
+++ manim.py
@@ -35,7 +35,7 @@ class TreeLayout(Scene):
 
         self.add(graph)
 
-class MinMaxTree(Scene):
+class DFS(Scene):
     def construct(self):
 
         self.camera.background_color = GRAY_A
@@ -90,4 +90,84 @@ class MinMaxTree(Scene):
 
         self.play(Create(manim_graph))
         self.wait()
+
+class MinMaxTree(Scene):
+    def construct(self):
+
+        self.camera.background_color = GRAY_A
+
+        graph_dict = {
+            "2_depth_0": ["2_depth_1", "-2_depth_1"],  # Use list instead of set
+            "2_depth_1": ["4_depth_2", "2_depth_2"],
+            "-2_depth_1": ["-4_depth_2", "-2_depth_2"],
+            "4_depth_2": [],
+            "2_depth_2": [],
+            "-2_depth_2": [],
+            "-4_depth_2": []
+        }
+
+        visited = []
+        def dfs(vertex, parent):
+            visited.append((vertex, parent))
+            for neighbor in graph_dict[vertex]:
+                if (neighbor, vertex) not in visited:
+                    dfs(neighbor, vertex)
+
+        dfs("2_depth_0", None)
+
+        vertices = [node for node, parent in visited]
+        edges = [(parent, node) for node, parent in visited if parent is not None]
         
+        even_depth_nodes_colors = {node: "WHITE" for node in graph_dict.keys() if int(node[-1]) % 2 == 0}
+        odd_depth_nodes_colors = {node: "BLACK" for node in graph_dict.keys() if int(node[-1]) % 2 != 0}
+
+        vertices_colors = even_depth_nodes_colors | odd_depth_nodes_colors
+        vertices_labels = {node: node[0] if node[0] != "-" else node[0:2] for node in graph_dict.keys()}
+
+        vertex_config = {
+            vertex: {
+                "fill_color": vertices_colors[vertex],
+                "label": vertices_labels[vertex],
+                "label_color": "WHITE" if vertices_colors[vertex] == "BLACK" else "BLACK"
+            } for vertex in vertices
+        }
+
+        graphs = []
+
+        for i in range(1, len(visited)):
+            vertices = [node for node, parent in visited[:i]]
+            edges = [(parent, node) for node, parent in visited[:i] if parent is not None]
+            
+            print("====")
+            print(vertices)
+            print(edges)
+
+            vertex_config = {
+                vertex: {
+                    "fill_color": vertices_colors[vertex],
+                    "label": vertices_labels[vertex],
+                    "label_color": "WHITE" if vertices_colors[vertex] == "BLACK" else "BLACK"
+                } for vertex in vertices
+            }
+
+            graph = DiGraph(
+                vertices,
+                edges,
+                layout="tree",
+                root_vertex="2_depth_0",
+                vertex_config={
+                    v: {"fill_color": vertex_config[v]["fill_color"]}
+                    for v in vertex_config
+                },
+                labels={v: Text(vertex_config[v]["label"], color=vertex_config[v]["label_color"]) for v in vertex_config}
+            )
+
+            graphs.append(graph)
+
+        self.play(Create(graphs[0]))
+        for i in range(1, len(graphs)):
+            print(graphs[i])
+
+            self.play(Transform(graphs[i - 1], graphs[i]))
+            self.wait()
+        self.wait()
