c10365de10f87c12a2da8ef739732f9589ee9c9b
MadeInShineA <olivier.amacker@netplus.ch>
2025-01-04 16:08:38 +0100

    Renamed pruning bot

--- Bots/AB_pruning.py
+++ Bots/AB_pruning.py
@@ -67,4 +67,4 @@ def chess_bot(player_sequence, actual_board, time_budget, **kwargs):
     finally:
         return best_move.start, best_move.end
 
-register_chess_bot("AB_pruning", chess_bot)
+register_chess_bot("ROBIN 0 COMMIT + 0 MOT DANS LE RAPPORT", chess_bot)
